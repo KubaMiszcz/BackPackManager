54aae5be49154dcce1daf7edd33ff78a8cd6d439
KubaMiszcz <zielonyeufor@gmail.com>
2023-03-07 17:31:02 +0100

    update models

--- BackPackManager-Front/src/app/models/item.ts
+++ BackPackManager-Front/src/app/models/item.ts
@@ -1,10 +1,23 @@
-interface Item {
+interface SimpleItem {
   name: string;
   description: string;
   image: any;
   weight: number;
   size: SizeEnum;
-  location: Item;
-  items: Item[];
+  location: StorageItem;
   priority: PriorityEnum[];
 }
+
+interface StorageItem extends SimpleItem {
+  items: SimpleItem[];
+  capacity: SizeEnum;
+  locationWhenNotused: StorageItem;
+}
+
+interface ToDoItem {
+  name: string;
+}
+  
+
+//generate tree
+//generate checklist
